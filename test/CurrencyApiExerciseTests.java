44ad58c34a32c609b35528e53295e4ba58e37b2e
fabian.vlasceanu <vlasceanufabian96@gmail.com>
2022-11-09 14:11:18 +0200

    Add more tests

--- test/CurrencyApiExerciseTests.java
+++ test/CurrencyApiExerciseTests.java
@@ -1,6 +1,11 @@
 import OpenCurrencyExercise.CurrencyLayerService;
 import OpenCurrencyExercise.QuotesModel;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.util.ArrayList;
 
@@ -12,9 +17,23 @@ public class CurrencyApiExerciseTests {
     @Test
     public void ShouldReturnAllQuates()
     {
-        CurrencyLayerService Sut = new CurrencyLayerService("980b4fe950b3d8d23a5a6eef697865af");
-        ArrayList<QuotesModel> quotes =  Sut.GetAllQuotes();
+        CurrencyLayerService currencyLayerService = new CurrencyLayerService("980b4fe950b3d8d23a5a6eef697865af");
+        ArrayList<QuotesModel> quotes = currencyLayerService.GetAllQuotes();
         assertNotNull(quotes);
         assertTrue(!quotes.stream().findAny().isEmpty());
+    }
+
+    @Test
+    public void ShouldReturnAllQuatesNamesAndValues()
+    {
+        CurrencyLayerService currencyLayerService = new CurrencyLayerService("980b4fe950b3d8d23a5a6eef697865af");
+        ArrayList<QuotesModel> quotes = currencyLayerService.GetAllQuotes();
+        assertNotNull(quotes);
+        for (QuotesModel quate:quotes
+             ) {
+
+            assertNotNull(quate.getQuatesName());
+            assertNotNull(quate.getQuatesValue());
+        }
     }
 }
